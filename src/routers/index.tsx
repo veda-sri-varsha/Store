70462ab9afc310941f4d5a9f7faaf19a300c2d1a
veda-sri-varsha <vedasrivarsha.kura@gmail.com>
2025-06-06 19:12:55 +0530

    refactor: router configuration

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -2,18 +2,15 @@ import { createBrowserRouter } from "react-router";
 import MainLayout from "../Components/layouts/MainLayout";
 import LoginPage from "../pages/LoginPage";
 import RegisterPage from "../pages/RegisterPage";
-import HomePage from "../pages/HomePage"; // for example
-// import ProductPage from "../pages/ProductPage"; // for example
-
+import HomePage from "../pages/HomePage"; 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
     children: [
-      { index: true, element: <HomePage /> }, // /
-      { path: "login", element: <LoginPage /> }, // /login
-      { path: "register", element: <RegisterPage /> }, // /register
-      // { path: "products", element: <ProductPage /> }, // /products
+      { index: true, element: <HomePage /> }, 
+      { path: "login", element: <LoginPage /> }, 
+      { path: "register", element: <RegisterPage /> },
     ],
   },
 ]);
